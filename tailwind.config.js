96c50d5ec472de1b282141b25ca33c88a175b763
Mateus Belicio <mateusbelicio@hotmail.com>
2023-08-28 18:58:57 -0300

    Feat: setting Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        md: '2.5rem',
+        xl: '0',
+      },
+    },
+    extend: {
+      colors: {
+        primary: '#4ce19e',
+        accent: '#ff6f5c',
+        neutral: {
+          100: '#ffffff',
+          200: '#d9d9d9',
+          300: '#808080',
+          400: '#404040',
+          500: '#242424',
+          600: '#141414',
+        },
+      },
+      fontFamily: {
+        sans: "'Space Grotesk', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
